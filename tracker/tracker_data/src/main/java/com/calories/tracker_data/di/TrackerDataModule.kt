6e49750f50c4e58414dda8125c4316e21d5fe16c
Igor Stevanovic <igorstevanovic99@gmail.com>
2022-01-29 18:43:03 +0100

    Added TrackerRepository

--- tracker/tracker_data/src/main/java/com/calories/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/calories/tracker_data/di/TrackerDataModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import com.calories.tracker_data.local.TrackerDatabase
 import com.calories.tracker_data.remote.OpenFoodApi
+import com.calories.tracker_data.repository.TrackerRepositoryImpl
+import com.calories.tracker_domain.repository.TrackerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -50,5 +52,17 @@ object TrackerDataModule {
             TrackerDatabase::class.java,
             "tracker_db"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerRepository(
+        api: OpenFoodApi,
+        db: TrackerDatabase
+    ): TrackerRepository {
+        return TrackerRepositoryImpl(
+            dao = db.dao,
+            api = api
+        )
     }
 }

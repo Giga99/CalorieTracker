d7b3ff8050cd6ea8b813319b54c4ce38da7ad0f7
Igor Stevanovic <igorstevanovic99@gmail.com>
2022-01-29 23:29:00 +0100

    Added SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/calories/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.calories.tracker_presentation.search
+
+import com.calories.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)

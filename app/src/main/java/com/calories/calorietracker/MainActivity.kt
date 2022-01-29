3e18d74a517507ec8288c8b3ec37497d72bdeb7a
Igor Stevanovic <igorstevanovic99@gmail.com>
2022-01-29 17:52:37 +0100

    Added NutrientGoalScreen

--- app/src/main/java/com/calories/calorietracker/MainActivity.kt
+++ app/src/main/java/com/calories/calorietracker/MainActivity.kt
@@ -18,6 +18,7 @@ import com.calories.onboarding_presentation.age.AgeScreen
 import com.calories.onboarding_presentation.gender.GenderScreen
 import com.calories.onboarding_presentation.goal.GoalScreen
 import com.calories.onboarding_presentation.height.HeightScreen
+import com.calories.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.calories.onboarding_presentation.weight.WeightScreen
 import com.calories.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -62,7 +63,10 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
 
                         composable(Route.ACTIVITY) {
